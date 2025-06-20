5aeb4f41b279d54a746ad4697b826ac168db9311
damini <damini.cs231079.global.org.in>
2025-06-20 10:22:13 +0530

    merge two sorted arrays  without using extra space

---
+++ merge.java
@@ -0,0 +1,48 @@
+public class merge {
+    
+    // Function to calculate next gap
+    private static int nextGap(int gap) {
+        if (gap <= 1) return 0;
+        return (gap / 2) + (gap % 2);
+    }
+
+    public static void merge(int[] arr1, int[] arr2) {
+        int n = arr1.length, m = arr2.length;
+        int gap = nextGap(n + m);
+
+        while (gap > 0) {
+            int i = 0, j = 0;
+
+            // Compare elements in the first array
+            for (i = 0; i + gap < n; i++) {
+                if (arr1[i] > arr1[i + gap]) {
+                    int temp = arr1[i];
+                    arr1[i] = arr1[i + gap];
+                    arr1[i + gap] = temp;
+                }
+            }
+
+            // Compare elements between both arrays
+            for (j = gap > n ? gap - n : 0; i < n && j < m; i++, j++) {
+                if (arr1[i] > arr2[j]) {
+                    int temp = arr1[i];
+                    arr1[i] = arr2[j];
+                    arr2[j] = temp;
+                }
+            }
+
+            // Compare elements in the second array
+            if (j < m) {
+                for (j = 0; j + gap < m; j++) {
+                    if (arr2[j] > arr2[j + gap]) {
+                        int temp = arr2[j];
+                        arr2[j] = arr2[j + gap];
+                        arr2[j + gap] = temp;
+                    }
+                }
+            }
+
+            gap = nextGap(gap);
+        }
+    }
+
