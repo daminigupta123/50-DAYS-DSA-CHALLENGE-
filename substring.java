f02042da8a4df5f9929fbeff54b7a215abf7a36b
Damini Gupta <damini.cs231079@global.org.in>
2025-06-23 16:00:51 +0530

    valid palindrome seacrch

--- substring.java
+++ substring.java
@@ -1,8 +1,5 @@
 public class substring {
-    
-
-
-    public static int strStr(String haystack, String needle) {
+       public static int strStr(String haystack, String needle) {
         if (needle == null || needle.isEmpty()) return 0;
         if (haystack == null || haystack.length() < needle.length()) return -1;
 
@@ -13,7 +10,6 @@ public class substring {
         }
         return -1;
     }
-
     public static void main(String[] args) {
         String haystack = "hello";
         String needle = "ll";
