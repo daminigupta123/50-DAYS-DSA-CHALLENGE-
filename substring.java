0d64c7dd569b7b51faf7fa7a0d2b54868492d2d7
Damini Gupta <damini.cs231079@global.org.in>
2025-06-23 15:47:08 +0530

    substring search

---
+++ substring.java
@@ -0,0 +1,22 @@
+public class substring {
+    
+
+
+    public static int strStr(String haystack, String needle) {
+        if (needle == null || needle.isEmpty()) return 0;
+        if (haystack == null || haystack.length() < needle.length()) return -1;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+        System.out.println(strStr(haystack, needle)); // Output: 2
+    }
+}
