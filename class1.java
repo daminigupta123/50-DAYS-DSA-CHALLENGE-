f9acc54e2e59804b9c4a65b94d0efd33316d16e8
Damini Gupta <damini.cs231079@global.org.in>
2026-03-10 12:33:46 +0530

    arraylist

--- class1.java
+++ class1.java
@@ -1,14 +1,21 @@
-
-
 import java.util.*;
-
 public class class1 {
     public static void main(String[] args) {
     ArrayList<Integer> list=new ArrayList<>();
-    list.add(1);
-    list.add(2);
-    list.add(3);
-    list.add(4);
+    list.add(11);
+    list.add(23);
+    list.add(68);
+    list.add(45);
+    list.set(3,67);
+    list.get(2);
+    list.remove(1);
     System.out.println(list);
+    System.out.println(list.size());
+    // for(int i=0;i<list.size();i++){
+        // System.out.println(list.get(i));
+    // }
+    for(int num:list){
+        System.out.println(num);
+    }
 }
 }
