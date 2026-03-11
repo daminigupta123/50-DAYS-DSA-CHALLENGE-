4fa86528cb34bd59cd7a852111bfe892d87d52cc
Damini Gupta <damini.cs231079@global.org.in>
2026-03-11 20:55:05 +0530

    n natural numbers

--- javapractice.java
+++ javapractice.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
 public class javapractice {
-    
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         // You can test the methods here
         // for(int i=0;i<=10;i++){
             // System.out.println(i);
         // }
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         int sum=0;
-        for(int i=1;i<=5;i++){
+        for(int i=1;i<=n;i++){
             sum=sum+i;
         }
-        System.out.println("Sum of first 5 natural numbers is: " + sum);
+         System.out.println("Sum of first " + n + " natural numbers is: " + sum);
     }
 }
